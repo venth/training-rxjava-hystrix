68096643fff8cee42fec4b62db1e2e0aa708e6ac
venth <artur.krysiak.protonmail.com>
2015-07-26 09:45:52 +0200

    added retrying functionality

--- src/main/java/org/venth/training/rxjavahystrix/ExternalSystemClient.java
+++ src/main/java/org/venth/training/rxjavahystrix/ExternalSystemClient.java
@@ -5,50 +5,69 @@ import rx.Observable;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author Venth on 23/07/2015
  */
 public class ExternalSystemClient {
-    static final int FIRST_NAME_SERVICE_TIMEOUT_IN_SECONDS = 5;
-    static final long LAST_NAME_SERVICE_TIMEOUT_IN_SECONDS = 10;
 
-    private final FirstNameService firstNameService;
-    private final LastNameService lastNameService;
+    static final int FIRST_NAME_SERVICE_TIMEOUT_IN_SECONDS = 1;
+
+    private FirstNameService firstNameService;
+    private int firstNameRetryTimes;
+    private LastNameService lastNameService;
+    private int lastNameRetryTimes;
 
     public ExternalSystemClient(FirstNameService firstNameService, LastNameService lastNameService) {
-
+        this();
         this.firstNameService = firstNameService;
         this.lastNameService = lastNameService;
+    }
+
+    public ExternalSystemClient() {
+        firstNameRetryTimes = 0;
+        lastNameRetryTimes = 0;
     }
 
     public String personName() {
         return createFetchFirstNameCommand()
                 .zipWith(createFetchLastNameCommand(), (firstName, lastName) -> firstName + lastName)
-                .firstOrDefault("not found")
                 .toBlocking()
-                .first();
+                .single();
     }
 
     private Observable<String> createFetchFirstNameCommand() {
-        return new HystrixCommand<String>(HystrixCommandGroupKey.Factory.asKey("person name services")) {
+        return Observable.defer(() ->
+                new HystrixCommand<String>(
+                    HystrixCommandGroupKey.Factory.asKey("person name services"),
+                    FIRST_NAME_SERVICE_TIMEOUT_IN_SECONDS * 1000
+                ) {
 
-            @Override
-            protected String run() throws Exception {
-                return firstNameService.firstName();
-            }
-        }.toObservable().timeout(FIRST_NAME_SERVICE_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
+                    @Override
+                    protected String run() throws Exception {
+                        return firstNameService.firstName();
+                    }
+
+                }.toObservable()).retry(firstNameRetryTimes);
     }
 
     private Observable<String> createFetchLastNameCommand() {
-        return new HystrixCommand<String>(HystrixCommandGroupKey.Factory.asKey("person name services")) {
+        return Observable.defer(() -> new HystrixCommand<String>(HystrixCommandGroupKey.Factory.asKey("person name services")) {
 
             @Override
             protected String run() throws Exception {
                 return lastNameService.lastName();
             }
-        }.toObservable().timeout(LAST_NAME_SERVICE_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
+        }.toObservable()).retry(lastNameRetryTimes);
+    }
+
+    public void setFirstNameService(FirstNameService firstNameService, int retryTimes) {
+        this.firstNameService = firstNameService;
+        this.firstNameRetryTimes = retryTimes;
+    }
+
+    public void setLastNameService(LastNameService lastNameService, int lastNameRetryTimes) {
+        this.lastNameService = lastNameService;
+        this.lastNameRetryTimes = lastNameRetryTimes;
     }
 }
 
--- src/test/java/org/venth/training/rxjavahystrix/ExternalSystemClientTest.groovy
+++ src/test/java/org/venth/training/rxjavahystrix/ExternalSystemClientTest.groovy
@@ -1,12 +1,69 @@
 package org.venth.training.rxjavahystrix
 
 import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.Supplier
+import java.util.stream.Stream
 
 /**
  * @author Venth on 25/07/2015
  */
 class ExternalSystemClientTest extends Specification {
+
+    final static Logger LOG = LoggerFactory.getLogger(ExternalSystemClientTest)
+
+    @Test
+    def "retry 4 time(s) in case of timeout"() {
+
+        given: "after 4 call(s) first name service returns a person name "
+        def retryTimesInCaseOfTimeout = 4
+        def personFirstName = "Luke"
+        def exceedTimeoutByASecond = (ExternalSystemClient.FIRST_NAME_SERVICE_TIMEOUT_IN_SECONDS + 1) * 1000
+        def firstNameService = Mock(FirstNameService)
+
+        firstNameService.firstName() >> {
+            LOG.debug("Latency appeared")
+            Thread.sleep(exceedTimeoutByASecond)
+
+            personFirstName
+        } >> {
+            LOG.debug("Latency appeared")
+            Thread.sleep(exceedTimeoutByASecond)
+
+            personFirstName
+        } >> {
+            LOG.debug("Latency appeared")
+            Thread.sleep(exceedTimeoutByASecond)
+
+            personFirstName
+        } >> {
+            LOG.debug("Latency appeared")
+            Thread.sleep(exceedTimeoutByASecond)
+
+            personFirstName
+        } >> personFirstName
+
+        and: "last name service returns luke's last name"
+        def personLastName = 'Skywalker'
+        def lastNameService = Mock(LastNameService)
+        lastNameService.lastName() >> personLastName
+
+        and: 'the system client uses external systems'
+        def client = new ExternalSystemClient()
+        client.setFirstNameService(firstNameService, retryTimesInCaseOfTimeout)
+        client.setLastNameService(lastNameService, 0)
+
+        when: "external services are called for person's name"
+        def personName = client.personName()
+
+        then: "because occurred timeouts on first name service one less than $retryTimesInCaseOfTimeout times person's name is combined"
+        "$personFirstName$personLastName" == personName
+    }
 
     @Test
     def "non blocking services are called in a parallel"() {
